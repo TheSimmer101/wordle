022ae30f384f1e83e7f4dd42c8d5254bd7240184
Maida <145588313+maida5@users.noreply.github.com>
2024-12-01 19:10:47 -0500

    fix words and guesses

--- wordle.cpp
+++ wordle.cpp
@@ -17,7 +17,7 @@ Wordle::Wordle()
     init_pair(1, COLOR_GREEN, COLOR_BLACK); // Pair 1: Green text on black background
     init_pair(2, COLOR_YELLOW, COLOR_BLACK);
     init_pair(3, COLOR_WHITE, COLOR_BLACK);
-    init_pair(4, COLOR_WHITE, COLOR_BLUE);  // Blue loading screen
+    init_pair(4, COLOR_WHITE, COLOR_BLUE); // Blue loading screen
 
     // CREATE POSSIBLE ANSWER HASHMAP
     std::ifstream infile; // creates variable for file to open for reading
@@ -38,8 +38,8 @@ Wordle::Wordle()
 
     // generate random position
     srand(time(0));
-    int randomPos = 1 + rand() % 106;
-   
+    int randomPos = 1 + rand() % 105;
+
     answer = possible_answers[randomPos];
     attempts = 0;
 
@@ -55,7 +55,6 @@ Wordle::Wordle()
     move(currentPos.y, currentPos.x);
     refresh();
 }
-
 
 std::string Wordle::getGuess(coordinates startPos)
 {
@@ -94,31 +93,19 @@ std::string Wordle::getGuess(coordinates startPos)
         {
             if (!hunspell.spell(guess))
             {
-              
+
                 move(startPos.y, startPos.x + 5);
                 printw("%s", "Please enter a valid word!");
                 refresh();
             }
             else
-            {  
+            {
                 move(startPos.y, startPos.x + 5);
                 clrtoeol();
                 refresh();
                 finalguess = true;
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
     }
     return guess;
 }
@@ -142,9 +129,8 @@ void Wordle::play()
     mvprintw(r - 6, c - 40, "If the letter is white: the letter is not in the word at all");
     mvprintw(r - 4, c - 40, "Have fun!");
 
-    
-    const char* ascii_art_2[] = {
-         ".      *    *           *.       *   .                      *     .",
+    const char *ascii_art_2[] = {
+        ".      *    *           *.       *   .                      *     .",
         "               .   .                   __   *    .     * .     *",
         "    *       *         *   .     .    _|__|_        *    __   .       *",
         "  .  *  /\\       /\\          *        ('')    *       _|__|_     .",
@@ -152,44 +138,41 @@ void Wordle::play()
         "  *    /  \\     /  \\   .   *       _(__.__)_  _   ,--<(  . )>  .    .",
         "      /    \\   /    \\          *   |       |  )),`   (   .  )     *",
         "   *   `||` ..  `||`   . *.   ... ==========='`   ... '--`-` ... * .",
-    nullptr
-    };
-
+        nullptr};
 
     // have the ascii snowman art be displayed at the bottom corner right
-    int offset_y1 = 3; 
-    int offset_x1 = 8; 
+    int offset_y1 = 3;
+    int offset_x1 = 8;
 
     int art_start_y1 = rows - 6 - offset_y1;
     int art_start_x1 = cols - strlen(ascii_art_2[0]) - offset_x1;
 
-    for (int i = 0; ascii_art_2[i] != nullptr; i++) {
+    for (int i = 0; ascii_art_2[i] != nullptr; i++)
+    {
         mvprintw(art_start_y1 + i, art_start_x1, ascii_art_2[i]);
     }
 
-    
-    const char* ascii_art_3[] = {
+    const char *ascii_art_3[] = {
         "                                                   __.  .--,",
-    "*-/___,  ,-/___,-/___,-/___,-/___,           _.-.=,{\\/ _/  /`)",
-    " `\\ _ ),-/___,-/___,-/___,-/___, )     _..-'`-(`._(_.;`   /",
-    "  /< \\\\=`\\ _ )`\\ _ )`\\ _ )`\\ _ )<`--''`     (__\\_________/___,",
-    "         /< <\\ </ /< /< /< </ /<           (_____Y_____Y___,",
-    
-        nullptr
-    };
+        "*-/___,  ,-/___,-/___,-/___,-/___,           _.-.=,{\\/ _/  /`)",
+        " `\\ _ ),-/___,-/___,-/___,-/___, )     _..-'`-(`._(_.;`   /",
+        "  /< \\\\=`\\ _ )`\\ _ )`\\ _ )`\\ _ )<`--''`     (__\\_________/___,",
+        "         /< <\\ </ /< /< /< </ /<           (_____Y_____Y___,",
+
+        nullptr};
 
     // have the ascii santa-sleigh art be displayed at the bottom corner left
-    int offset_y2 = 0; 
-    int offset_x2 = 2; 
+    int offset_y2 = 0;
+    int offset_x2 = 2;
 
     int art_start_y2 = rows - 6 - offset_y2;
     int art_start_x2 = offset_x2;
 
-    for (int i = 0; ascii_art_3[i] != nullptr; i++) {
+    for (int i = 0; ascii_art_3[i] != nullptr; i++)
+    {
         mvprintw(art_start_y2 + i, art_start_x2, ascii_art_3[i]);
     }
 
-    
     char dash[dash_length + 1];
     for (size_t i = 0; i < answer.length(); ++i)
     {
@@ -201,7 +184,7 @@ void Wordle::play()
     }
     dash[dash_length] = '\0';
 
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < answer.length(); ++i)
     {
         mvprintw(r + i, c, "%s", dash);
     }
@@ -211,8 +194,8 @@ void Wordle::play()
     move(currentPos.y, currentPos.x);
     refresh();
 
-//main code for getting guess & ensuring it's valid, then printing with colors
-    for (int i = 1; i <= 5; i++)
+    // main code for getting guess & ensuring it's valid, then printing with colors
+    for (int i = 1; i <= answer.length(); i++)
     {
         guess = getGuess(currentPos);
         std::vector<Wordle::colors> colorVec = getColors(guess);
@@ -261,18 +244,16 @@ void Wordle::play()
             currentPos.x = c;
             currentPos.y = r + i;
             move(currentPos.y, currentPos.x);
-
         }
     }
 
-
     int centerCol = cols / 2;
 
-    if (guess == answer) 
+    if (guess == answer)
     {
         mvprintw(r + 7, centerCol - 16, "Yay! You guessed correctly! :D");
-    } 
-    else 
+    }
+    else
     {
         mvprintw(r + 6, centerCol - 13, "Aw, you didn't get the word!");
         std::string revealAnswer = "The word is: " + answer;
@@ -283,26 +264,17 @@ void Wordle::play()
     mvprintw(r + 9, centerCol - prompt.length() / 2, "%s", prompt.c_str());
 
     const char *choices[] =
-    {
-        "Play Again",
-        "Quit",
-    };
+        {
+            "Play Again",
+            "Quit",
+        };
 
     currentPos.y = r + 10;
     currentPos.x = centerCol - prompt.length() + 24;
 
-    mvprintw(currentPos.y,currentPos.x, "%s", choices[0]);
-    mvprintw(currentPos.y + 1,currentPos.x + 3, "%s", choices[1]);
-
-   
-
-    
-
-    
-       
+    mvprintw(currentPos.y, currentPos.x, "%s", choices[0]);
+    mvprintw(currentPos.y + 1, currentPos.x + 3, "%s", choices[1]);
 }
-
-
 
 void Wordle::loadScreen()
 {
@@ -313,12 +285,12 @@ void Wordle::loadScreen()
 
     WINDOW *windowTest = newwin(30, 100, 6, 60);
     int text_startx = (60 - strlen("hello world")) / 2; // put some text to help with centering
-    
+
     mvwprintw(windowTest, 1, text_startx - 7, "Welcome to Winter Wordleland!");
     mvwprintw(windowTest, 3, text_startx - 4, "Loading... Please wait");
 
-   // display this cute winter scenery in loading page
-   const char* ascii_art_1[] = {
+    // display this cute winter scenery in loading page
+    const char *ascii_art_1[] = {
         "   .-.                                                   \\ /",
         "  ( (                                |                  - * -",
         "   '-`                              -+-                  / \\",
@@ -328,12 +300,11 @@ void Wordle::loadScreen()
         "     ___ /_________\\   //|\\\\   /___________\\  /_________\\",
         "    /___\\ |[] _ []|    //|\\\\    | A /^\ A |    |[] _ []| _.O,_",
         "....|\"#\"|.|  |*|  |...///|\\\\\\...|   |\"|   |....|  |*|  |..(^)....",
-        nullptr
-    };
-   
+        nullptr};
 
     int start_y = 10;
-    for (int i = 0; ascii_art_1[i] != nullptr; i++) {
+    for (int i = 0; ascii_art_1[i] != nullptr; i++)
+    {
         mvwprintw(windowTest, start_y + i, 0, ascii_art_1[i]);
     }
 
@@ -352,66 +323,60 @@ void Wordle::loadScreen()
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    
+
     werase(windowTest);
     wrefresh(windowTest);
     delwin(windowTest);
     refresh();
-  
 }
 
 std::vector<Wordle::colors> Wordle::getColors(const std::string &guess) const
 {
-    //list the quantity of each letter present in answer and guess, (e.g. candy has 1 c, 1 a, 1 n, etc.)
+    // list the quantity of each letter present in answer and guess, (e.g. candy has 1 c, 1 a, 1 n, etc.)
     std::unordered_map<char, int> answerLettersCount;
     std::unordered_map<char, int> guessLettersCount;
 
-    for(int x = 0; x < guess.length();x++)
+    for (int x = 0; x < guess.length(); x++)
     {
         answerLettersCount[answer[x]]++;
         guessLettersCount[guess[x]]++;
     }
-    std::vector<Wordle::colors> result(guess.length(),white); //declares vector of size guessLength and fills it with white
+    std::vector<Wordle::colors> result(guess.length(), white); // declares vector of size guessLength and fills it with white
 
-//TESTING
-// answer: JOLLY  (2 L's), this is already set in constructor rn
-// guess: LOLLY   (3 L's)
-// colors should be: 1st L is WHITE, everything else GREEN
-//problem: 1st L is yellow, which is wrong. (OLLY is green, which is good!)
-//yellow implies you can move that letter somewhere else to get answer, but yellow L then all green OLLY contradicts that idea.
-//only the 1st letter is wrong, but you can't move it to another position since we know OLLY is the fully correct part
+    // TESTING
+    //  answer: JOLLY  (2 L's), this is already set in constructor rn
+    //  guess: LOLLY   (3 L's)
+    //  colors should be: 1st L is WHITE, everything else GREEN
+    // problem: 1st L is yellow, which is wrong. (OLLY is green, which is good!)
+    // yellow implies you can move that letter somewhere else to get answer, but yellow L then all green OLLY contradicts that idea.
+    // only the 1st letter is wrong, but you can't move it to another position since we know OLLY is the fully correct part
 
-
-
-//if letter is in the right spot mark it green, and decrement that letter count in guess map
-// green letters have priority
+    // if letter is in the right spot mark it green, and decrement that letter count in guess map
+    //  green letters have priority
     for (int k = 0; k < guess.length(); k++)
     {
-        if(guess[k] == answer[k])
+        if (guess[k] == answer[k])
         {
             result[k] = green;
             answerLettersCount[guess[k]]--;
         }
-            
-        
+
         // might need to modify case for yellow, for example answer = "jolly", and guess = "local", both l's would need to be yellow
-          
-                
-        
+
         // else if (charIndex(answer, letter) == k)
         //     result.push_back(green);
     }
     for (int j = 0; j < guess.length(); j++)
     {
-        //letter not in right position and quantity of letter in guess is less than its quantity in answer
-        if (guess[j] != answer[j] && guessLettersCount[guess[j]]!= 0 && guessLettersCount[guess[j]] <= answerLettersCount[guess[j]]) 
-            {
-                
-                result[j] = yellow;
-                guessLettersCount[guess[j]]--;
-            }
+        // letter not in right position and quantity of letter in guess is less than its quantity in answer
+        if (guess[j] != answer[j] && guessLettersCount[guess[j]] != 0 && guessLettersCount[guess[j]] <= answerLettersCount[guess[j]])
+        {
+
+            result[j] = yellow;
+            guessLettersCount[guess[j]]--;
+        }
     }
-    
+
     return result;
 }
 
